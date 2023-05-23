a417d40e8d0de6a315f1b731f6d6e3984efa718a
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-23 11:32:59 -0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>React App</h1>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import DetailsPage from './components/DetailsPage';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/details/:id" element={<DetailsPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
