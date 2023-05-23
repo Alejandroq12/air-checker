ec7f02fc947a699a267d9a855d3bf6e17642a895
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-23 11:31:52 -0600

    fetch data correctly

--- src/api.js
+++ src/api.js
@@ -8,19 +8,22 @@ const apiInstance = axios.create({
 });
 
 export const getCurrentAirPollution = (lat, lon) => {
-  return apiInstance.get(
-    `/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-  );
+  if (typeof lat !== 'number' || typeof lon !== 'number') {
+    return Promise.reject(new Error('Latitude and longitude must be numbers'));
+  }
+
+  return apiInstance.get(`/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
 };
 
-export const getForecastAirPollution = (lat, lon) => {
-  return apiInstance.get(
-    `/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`
-  );
-};
+export const getForecastAirPollution = (lat, lon) => apiInstance.get(
+  `/air_pollution/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`,
+);
 
-export const getHistoricalAirPollution = (lat, lon, start, end) => {
-  return apiInstance.get(
-    `/air_pollution/history?lat=${lat}&lon=${lon}&start=${start}&end=${end}&appid=${API_KEY}`
-  );
+export const getHistoricalAirPollution = (lat, lon, start, end) => apiInstance.get(
+  `/air_pollution/history?lat=${lat}&lon=${lon}&start=${start}&end=${end}&appid=${API_KEY}`,
+);
+
+export const fetchWeatherData = async (city) => {
+  const response = await apiInstance.get(`/weather?q=${city}&appid=${API_KEY}`);
+  return response.data;
 };
