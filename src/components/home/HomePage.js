36865f854046f5c802019d257c3b19174286e6d2
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-24 16:29:53 -0600

    Update Home Page location

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,43 @@
+// HomePage.js
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchCurrentAirPollution } from '../airPollutionSlice';
+import LOCATIONS from '../Locations';
+import Filter from '../Filter';
+import './HomePage.css';
+
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const [filter, setFilter] = useState('');
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredLocations = LOCATIONS.filter(
+    (location) => location.name.toLowerCase().includes(filter.toLowerCase()),
+  );
+
+  return (
+    <div>
+      <div className="banner" />
+      <Filter className="search" value={filter} onChange={handleFilterChange} />
+      <div className="container">
+        {filteredLocations.map((location) => (
+          <Link key={location.name} to={`/details/${location.name}`}>
+            <button
+              className="button"
+              type="button"
+              onClick={() => dispatch(fetchCurrentAirPollution(location))}
+            >
+              {location.name}
+            </button>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
