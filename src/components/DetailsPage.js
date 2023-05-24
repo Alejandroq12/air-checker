d97ce8aa84c84ac29d2d2711723e121791a58802
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-24 13:41:18 -0600

    Implement state management

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -4,3 +4,23 @@ import { useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCurrentAirPollution, selectData } from './airPollutionSlice';
 import LOCATIONS from './Locations';
+
+const DetailsPage = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const location = LOCATIONS.find((location) => location.name === id);
+    if (location) {
+      dispatch(fetchCurrentAirPollution(location));
+    }
+  }, [dispatch, id]);
+
+  const data = useSelector((state) => selectData(state)[id]);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  const item = data[0];
+  
