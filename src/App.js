1c6a54b945f37f7a9d6ff4f737a79bd03b2075ca
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-24 16:27:10 -0600

    Update imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import DetailsPage from './components/DetailsPage';
+import HomePage from './components/home/HomePage';
+import DetailsPage from './components/details/DetailsPage';
 
 const App = () => (
   <Router>
