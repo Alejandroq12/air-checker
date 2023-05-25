c00eb36dcae44288433438ba99dacedcbe975760
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-24 19:00:31 -0600

    Add span in Filter.js

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -6,6 +6,7 @@ const Filter = ({ value, onChange }) => (
   <div className="filter-container">
     <label htmlFor="filter" className="filter-label">
       Filter by name:
+      <span> </span>
       <input className="filter-input" id="filter" type="text" value={value} onChange={onChange} />
     </label>
   </div>
