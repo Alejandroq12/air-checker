a3d6601998e18b3ab71dd6a5f5e079482936c28e
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-25 10:31:44 -0600

    Add navigation var in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/home/HomePage';
 import DetailsPage from './components/details/DetailsPage';
+import NavBar from './components/navbar/NavBar';
 
 const App = () => (
   <Router>
+    <NavBar />
     <Routes>
       <Route path="/details/:id" element={<DetailsPage />} />
       <Route path="/" element={<HomePage />} />
